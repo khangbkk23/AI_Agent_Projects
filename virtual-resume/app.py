3aed092a40074af851617d8b64e68da05e167401
khangbkk23 <khang.buitranduycse@hcmut.edu.vn>
2026-01-20 11:02:20 +0700

    done the chatbot tool for retrieve my resume

--- virtual-resume/app.py
+++ virtual-resume/app.py
@@ -68,3 +68,73 @@ record_unknown_question_json = {
 tools = [{"type": "function", "function": record_user_details_json},
         {"type": "function", "function": record_unknown_question_json}]
 
+class Me:
+    def __init__(self):
+        self.openai = OpenAI()
+        self.name = "Khang Bui Tran Duy"
+        reader = PdfReader("../resume/resume_khangbuitranduy_v1.2_updated.pdf")
+        self.resume = ""
+        for page in reader.pages:
+            text = page.extract_text()
+            if text:
+                self.resume += text
+        
+        with open("../resume/summary.txt", "r", encoding="utf-8") as f:
+            self.summary = f.read()
+            
+    def handle_tool_call(self, tool_calls):
+        results = []
+        for tool_call in tool_calls:
+            tool_name = tool_call.function.name
+            arguments = json.loads(tool_call.function.arguments)
+            print(f"Tool called: {tool_name}", flush=True)
+            tool = globals().get(tool_name)
+            result = tool(**arguments) if tool else {}
+            results.append({"role": "tool","content": json.dumps(result),"tool_call_id": tool_call.id})
+        return results
+    
+    def system_prompt(self):
+        system_prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website, \
+particularly questions related to {self.name}'s career, background, skills and experience. \
+Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \
+You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
+Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
+If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
+
+        system_prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
+        system_prompt += f"With this context, please chat with the user, always staying in character as {self.name}."
+        return system_prompt
+    
+    def chat(self, message, history):
+        # 1. Khởi tạo system prompt
+        messages = [
+            {"role": "system", "content": self.system_prompt()},
+        ]
+    
+        messages.extend(history)
+        messages.append({"role": "user", "content": message})
+        response = self.openai.chat.completions.create(
+            model="gpt-4o-mini", 
+            messages=messages,
+            tools=tools,
+            tool_choice="auto"
+        )
+
+        assistant_message = response.choices[0].message
+
+        if assistant_message.tool_calls:
+            messages.append(assistant_message)
+            tool_results = self.handle_tool_call(assistant_message.tool_calls)
+            messages.extend(tool_results)
+            second_response = self.openai.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=messages
+            )
+            return second_response.choices[0].message.content
+        return assistant_message.content
+
+if __name__ == "__main__":
+    me = Me()
+    gr.ChatInterface(me.chat, type="messages").launch()
+        
